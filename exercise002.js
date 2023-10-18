5f06e404891cca2d8c2b29d065ee84b635a5de41
vswapna3202 <vswapna3202@gmail.com>
2023-10-18 19:02:36 +0100

    Refactor: tidied code

--- exercise002.js
+++ exercise002.js
@@ -1,23 +1,25 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
-/*
+
 const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
     .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((error) => console.log(error));
 };
-*/
-const fetchData = async(apiEndPoint) => {  
+
+fetchData(jsonTypicode);
+
+const asyncfetchData = async(apiEndPoint) => {  
   try{
-    const response = await fetch(apiEndPoint);
-    const json = await response.json();
+    const awaitResponse = await fetch(apiEndPoint);
+    const awaitJson = await awaitResponse.json();
  
-    console.log(`JSON: `,json);
-  }catch(error){
-    console.log(`Error: ${error}`);
+    console.log(awaitJson);
+  }catch(asyncError){
+    console.log(`Error: ${asyncError}`);
   }
 }
 
-fetchData(jsonTypicode);
+asyncfetchData(jsonTypicode);
