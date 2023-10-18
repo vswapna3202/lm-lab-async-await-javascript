2baabd99beca7f4e68518f8e43ed8bf49ade1845
vswapna3202 <vswapna3202@gmail.com>
2023-10-18 16:31:14 +0100

    Feat: Completed exercise002.js

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,23 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
-
+/*
 const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
     .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((error) => console.log(error));
 };
+*/
+const fetchData = async(apiEndPoint) => {  
+  try{
+    const response = await fetch(apiEndPoint);
+    const json = await response.json();
+    console.log(`Response: `,response);    
+    console.log(`JSON: `,json);
+  }catch(error){
+    console.log(`Error: ${error}`);
+  }
+}
 
 fetchData(jsonTypicode);
